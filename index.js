4bfb5ec44e6115948fa56d6e21d3cc1b0e29871a
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-02-09 12:02:09 +0000

    Move setting of Form.Error to top-level file
    This makes it more explicit what's going on, and isn't needed internally.

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var Form = require('./lib/form');
 Form.validators = require('./lib/validation/validators');
 Form.formatters = require('./lib/formatting/formatters');
 
+Form.Error = require('./lib/error');
+
 module.exports = Form;
