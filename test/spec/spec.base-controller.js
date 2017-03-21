7d2cfb77e09507823d826baf9a43d48797bbfaa8
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2017-03-21 16:14:52 +0000

    feature - added _filterFields pipeline method
    * added _filterFields private method to get and post pipelines, after configure.
    * removed useWhen functionality from controller - this is now covered in filterFields in baseController

--- test/spec/spec.base-controller.js
+++ test/spec/spec.base-controller.js
@@ -8,7 +8,7 @@ const FormError = require('../../lib/base-error');
 const _ = require('lodash');
 const EventEmitter = require('events').EventEmitter;
 
-describe('Form Controller', () => {
+describe.only('Form Controller', () => {
 
   beforeEach(() => {
     sinon.spy(EventEmitter.prototype, 'emit');
@@ -209,6 +209,77 @@ describe('Form Controller', () => {
       form._configure(req, res, cb);
       req.form.options.fields.field.should.equal('mutated');
       form.options.fields.field.should.equal('name');
+    });
+
+  });
+
+  describe('filterFields', () => {
+
+    let form;
+    let req;
+    let res;
+    let cb;
+
+    beforeEach(() => {
+      form = new Form({
+        template: 'index',
+        next: '/next',
+        fields: {
+          field: 'name'
+        }
+      });
+      req = request({
+        path: '/index',
+        baseUrl: '/base',
+        form: {
+          options: {}
+        }
+      });
+      res = {
+        render: sinon.stub(),
+        locals: {}
+      };
+      cb = sinon.stub();
+      sinon.spy(form, '_filterFields');
+      sinon.stub(form, 'filterFields').yields();
+    });
+
+    it('is called as part of `get` pipeline', () => {
+      form.get(req, res, cb);
+      form._filterFields.should.have.been.calledOnce.and.calledWith(req, res);
+    });
+
+    it('is called as part of `post` pipeline', () => {
+      form.post(req, res, cb);
+      form._filterFields.should.have.been.calledOnce.and.calledWith(req, res);
+    });
+
+    it('calls through to form.filterFields', () => {
+      form._filterFields(req, res, cb);
+      form.filterFields.should.have.been.calledOnce.and.calledWith(req, res, cb);
+    });
+
+    it('filters fields if useWhen is provided and not satisfied', () => {
+      req.form.options.fields = {
+        'a-field': {
+          useWhen: {
+            field: 'b-field',
+            value: 'true'
+          }
+        },
+        'another-field': {
+          useWhen: 'c-field'
+        }
+      };
+      req.sessionModel = {
+        get: sinon.stub(),
+        unset: sinon.stub()
+      };
+      req.sessionModel.get.withArgs('b-field').returns('false');
+      req.sessionModel.get.withArgs('c-field').returns('true');
+      form._filterFields(req, res, cb);
+      req.form.options.fields.should.not.have.property('a-field');
+      req.form.options.fields.should.have.property('another-field');
     });
 
   });
